2df70285907febe84080a9e5ab98d2fa82988c5d
Abdulla Al Masud <info.abdullaalmasudbd@gmail.com>
2024-12-03 23:41:05 +0600

    problem

---
+++ problemSolve.js
@@ -0,0 +1,81 @@
+// Problem 1
+
+let carName = "Volvo";
+console.log(carName)
+
+// Problem 2
+
+let firstName = "Hablu";
+let lastName = "Programmer";
+let age = 35;
+
+// Problem 3
+
+x = 10;
+y = 5;
+x = x * y;
+console.log(x); // Output: 50
+
+
+// Problem 4
+
+let length = 16; // Number
+let lastNames = "Hablu"; // String
+
+const x = {
+  firstName: "Hablu",  
+  lastName: "Programmer"
+}; // Object
+
+// problem 5
+
+function myFunction() {
+  alert("Hablu Programmer");
+}
+myFunction();
+
+// Problem 6
+
+let person = {
+  name: "Hablu Programmer",
+  age: 50
+};
+alert(`${person.name} is ${person.age}`);
+
+// Problem 7
+// Chack Html File 
+
+// Problem 8
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);
+
+
+const Brand = ["Volvo", "Jeep", "Mercedes"];
+Brand[0] = "Ford";
+console.log(Brand);
+
+
+// Problem 9
+
+const randomNumber = Math.random();
+console.log(randomNumber);
+
+
+const large = Math.max(10, 20);
+console.log(large);
+
+
+const square = Math.sqrt(9);
+console.log(square);
+
+// Problem 10
+
+const x = 10;
+const y = 5;
+alert(x > y); 
+// true 
+
+const age = 17;
+alert(age < 18 ? "Too young" : "Old enough"); 
+
